e887ad62b0716ee12442b47ddef7d33d6dd888cc
Leandro Biazon <leandrobiazon@gmail.com>
2013-08-02 18:44:17 -0300

    MusicalInstrument communicates with external PdEngine App, via Intents

--- appinventor/components/src/com/google/appinventor/components/runtime/MusicalInstrument.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/MusicalInstrument.java
@@ -15,6 +15,10 @@ import org.puredata.android.io.PdAudio;
 import org.puredata.android.utils.PdUiDispatcher;
 import org.puredata.core.PdBase;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -39,7 +43,7 @@ import com.google.appinventor.components.runtime.util.ErrorMessages;
 
 /**
  * Component for synthetizing sounds
- * 
+ *
  * @author biazon@usp.br (Leandro Biazon)
  */
 @DesignerComponent(version = YaVersion.MUSICALINSTRUMENT_COMPONENT_VERSION, description = "<p>A non-visible component interfaces "
@@ -47,21 +51,39 @@ import com.google.appinventor.components.runtime.util.ErrorMessages;
 @SimpleObject
 // @UsesPermissions(permissionNames = "android.permission.INTERNET")
 @UsesLibraries(libraries = "pdcore.jar")
-public final class MusicalInstrument extends AndroidNonvisibleComponent {
+public final class MusicalInstrument extends AndroidNonvisibleComponent implements Component {
 
   // the following fields should only be accessed from the UI thread
   private String helloComponent = "";
   private final SharedPreferences sharedPreferences;
   private final ComponentContainer container;
+  private final Activity activity;
+  private final int sampleRate;
 
   private PdUiDispatcher dispatcher;
 
   public MusicalInstrument(ComponentContainer container) {
     super(container.$form());
     this.container = container;
+    this.activity = container.$context();
+    this.sampleRate = AudioParameters.suggestSampleRate();
+    this.dispatcher = new PdUiDispatcher();
 
     sharedPreferences = container.$context().getSharedPreferences("MusicalInstrument",
         Context.MODE_PRIVATE);
+  }
+
+  private void showNoticeAndDie(String message, String title, String buttonText){
+    AlertDialog alertDialog = new AlertDialog.Builder(activity).create();
+    alertDialog.setTitle(title);
+    // prevents the user from escaping the dialog by hitting the Back button
+    alertDialog.setCancelable(false);
+    alertDialog.setMessage(message);
+    alertDialog.setButton(buttonText, new DialogInterface.OnClickListener() {
+      public void onClick(DialogInterface dialog, int which) {
+        activity.finish();
+      }});
+    alertDialog.show();
   }
 
   /**
@@ -73,8 +95,16 @@ public final class MusicalInstrument extends AndroidNonvisibleComponent {
   }
 
   /**
+   * sampleRate property getter method.
+   */
+  @SimpleProperty(category = PropertyCategory.BEHAVIOR)
+  public String SampleRate() {
+    return sampleRate;
+  }
+
+  /**
    * helloComponent property setter method.
-   * 
+   *
    * @param helloComponent
    *          simple parameter for testing
    */
@@ -84,40 +114,70 @@ public final class MusicalInstrument extends AndroidNonvisibleComponent {
     this.helloComponent = helloComponent;
   }
 
-  /**
-   * Starts pure data engine
-   */
-  @SimpleFunction(description = "Starts pure data engine")
-  public void initPd() {
+  @SimpleFunction(description = "Set receiver")
+  public void setReceiver() {
     try {
-      // configure the audio glue
-      int sampleRate = AudioParameters.suggestSampleRate();
-      PdAudio.initAudio(sampleRate, 0, 2, 8, true);
-      
-      // create and install the dispatcher
-      dispatcher = new PdUiDispatcher();
       PdBase.setReceiver(dispatcher);
-      
-      form.dispatchErrorOccurredEvent(this, "initPd",
-          ErrorMessages.ERROR_MUSICALINSTRUMENT, String.valueOf(sampleRate));
-      
-    } catch (IOException e) {
-        form.dispatchErrorOccurredEvent(this, "initPd",
-            ErrorMessages.ERROR_MUSICALINSTRUMENT, "FUUUUU");
+    } catch (Exception e) {
+      showNoticeAndDie(
+          "The receiver died.",
+          "libpd",
+          "Ow man...");
     }
   }
-  
-  @SimpleFunction(description = "Checks if pd engine is running") 
+
+  @SimpleFunction(description = "Init audio")
+  public void initAudio() {
+    try {
+      PdAudio.initAudio(sampleRate, 0, 2, 8, true);
+    } catch (Exception e) {
+      showNoticeAndDie(
+          "The audio do not want to start.",
+          "libpd",
+          "Ow man...");
+    }
+  }
+
+  @SimpleFunction(description = "Init audio")
+  public void TriggerNote(int note) {
+    Log.d("App Inventor", "Note sent :" + note);
+    Intent intent = new Intent();
+    intent.setAction("AppInventor");
+    intent.putExtra("note", note);
+    activity.sendBroadcast(intent);
+  }
+
+  @SimpleFunction(description = "Checks if pd engine is running")
   public void isPdRunning() {
     final String isRunning = String.valueOf(PdAudio.isRunning());
     form.dispatchErrorOccurredEvent(this, "isPdRunning",
         ErrorMessages.ERROR_MUSICALINSTRUMENT, isRunning);
   }
-  
-  @SimpleFunction(description = "Shows sample rate") 
+
+  @SimpleFunction(description = "Shows sample rate")
   public void showSampleRate() {
     final String sampleRate = String.valueOf(AudioParameters.suggestSampleRate());
     form.dispatchErrorOccurredEvent(this, "showSampleRate",
         ErrorMessages.ERROR_MUSICALINSTRUMENT, sampleRate);
   }
+
+  /**
+   * Starts pure data engine
+   */
+  /*
+  @SimpleFunction(description = "Starts pure data engine")
+  public void initPd() {
+    try {
+      PdAudio.initAudio(sampleRate, 0, 2, 8, true);
+      PdBase.setReceiver(dispatcher);
+
+      form.dispatchErrorOccurredEvent(this, "initPd",
+          ErrorMessages.ERROR_MUSICALINSTRUMENT, String.valueOf(sampleRate));
+
+    } catch (Exception e) {
+        form.dispatchErrorOccurredEvent(this, "initPd",
+            ErrorMessages.ERROR_MUSICALINSTRUMENT, "FUUUUU");
+    }
+  }
+  */
 }
